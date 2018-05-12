ada145b3f83c7358bbe53601dca1ca0fbf8c6130
Subhajitdas298 <subhajitdas298@live.com>
2018-05-12 22:52:46 +0530

    Updated sql file

---
+++ sql/test.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2018 at 07:21 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(10) NOT NULL,
+  `callno` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `publisher` varchar(100) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `issued` int(10) NOT NULL,
+  `added_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `callno`, `name`, `author`, `publisher`, `quantity`, `issued`, `added_date`) VALUES
+(1, 'A@4', 'C In Depth', 'Shrivastav', 'BPB', 2, 0, '2018-05-12 16:41:04'),
+(2, 'B@1', 'DBMS', 'Korth', 'Pearson', 2, 1, '2018-05-12 16:42:32'),
+(3, 'G@12', 'Let\'s see', 'Yashwant Kanetkar', 'BPB', 9, 1, '2018-05-12 16:42:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issuebooks`
+--
+
+CREATE TABLE `issuebooks` (
+  `id` int(11) NOT NULL,
+  `bookcallno` varchar(50) NOT NULL,
+  `studentid` int(11) NOT NULL,
+  `issueddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `issuebooks`
+--
+
+INSERT INTO `issuebooks` (`id`, `bookcallno`, `studentid`, `issueddate`) VALUES
+(8, 'B@1', 6, '2018-05-12 16:39:39'),
+(9, 'G@12', 4, '2018-05-12 16:40:38');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE `librarian` (
+  `id` int(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `librarian`
+--
+
+INSERT INTO `librarian` (`id`, `name`, `password`, `email`, `address`, `city`, `contact`) VALUES
+(1, 'sumedh', 'sumesh', 'sumesh@gmail.com', 'Kuch Bhi', 'noida', '9382393282'),
+(7, 'ko', 'la', 'kola@gmail.com', 'WB, India', 'Srirampore', '8652456987');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(5) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `name`, `email`, `address`, `city`, `contact`) VALUES
+(1, 'Subhajit', 'sd298@gmail.com', 'wb,india', 'janai', '9998328238'),
+(4, 'Sayani', 'sumesh@gmail.com', 'wb,india', 'kolkata', '9007026325'),
+(6, 'Abhisekh', 'abhi@gmail.com', 'wb,india', 'kolkata', '9239328232');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `callno` (`callno`),
+  ADD UNIQUE KEY `callno_2` (`callno`);
+
+--
+-- Indexes for table `issuebooks`
+--
+ALTER TABLE `issuebooks`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studentid` (`studentid`),
+  ADD KEY `bookcallno` (`bookcallno`);
+
+--
+-- Indexes for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `issuebooks`
+--
+ALTER TABLE `issuebooks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `librarian`
+--
+ALTER TABLE `librarian`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `issuebooks`
+--
+ALTER TABLE `issuebooks`
+  ADD CONSTRAINT `issuebooks_ibfk_1` FOREIGN KEY (`studentid`) REFERENCES `student` (`id`),
+  ADD CONSTRAINT `issuebooks_ibfk_2` FOREIGN KEY (`bookcallno`) REFERENCES `books` (`callno`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
